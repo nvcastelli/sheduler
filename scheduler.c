b8171ab891f60b2485c005aab03ab588eac2c059
nvcastelli <nvc11@my.fsu.edu>
2014-10-29 17:49:30 -0400

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -3,47 +3,54 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-const double child = 0.5;
-const double adult = 1;
-const double adultLuggage = 2;
-//const int shuttleCapacity = 25;
+
+#define CHILD 1
+#define ADULT 2
+#define ADULT_LUGGAGE 4
+
+enum status = {PARKED, MOVING, DEACTIVATING, OFFLINE};
 
 struct shuttle{
-  const int shuttleCapacity;
-  bool status;
-};
+	#define SHUTTLECAPACITY 50
+	int startTerminal = 3;
+	int currentPassengers = 0;
+	int status = 3;//false = parked true = moving
+} theShuttle;
+
+struct passenger{
+	int passengerTyper;
+	int startTerminal;
+	int destinationTerminal;
+} person;
+
 
 MODULE_LICENSE("GPL");
 
-/*int start_shuttle(void) 
-A shuttle begins at Terminal C with no passenger onboard
- and no passenger waiting at all terminals,
- and with a shuttle status of either PARKED or MOVING.  
- The call returns 1 if the service has already been started 
- and 0 if it was started successfully.*/
-
 static int start_shuttle(struct seq_file *m, void *v) {
+	
 
-  seq_printf(m, "namaste");
+	if(theShuttle.status == 1 || theShuttle.status == 0)
+		return 1;
 
-  return 0;
+seq_printf(m, "The shuttle is starting at terminal %d", theShuttle.startTerminal);
+
+return 0;
 }
-
-static int hello_proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, start_shuttle, NULL);
+static int hello_proc_open(struct inode *inode, struct file *file) {
+return single_open(file, start_shuttle, NULL);
 }
 static const struct file_operations hello_proc_fops = {
-  .owner = THIS_MODULE,
-  .open = hello_proc_open,
-  .read = seq_read,
-  .release = single_release,
+.owner = THIS_MODULE,
+.open = hello_proc_open,
+.read = seq_read,
+.release = single_release,
 };
 static int __init hello_proc_init(void) {
-  proc_create("terminal", 0, NULL, &hello_proc_fops);
-  return 0;
+proc_create("terminal", 0, NULL, &hello_proc_fops);
+return 0;
 }
 static void __exit hello_proc_exit(void) {
-  remove_proc_entry("terminal", NULL);
+remove_proc_entry("terminal", NULL);
 }
 module_init(hello_proc_init);
 module_exit(hello_proc_exit);
